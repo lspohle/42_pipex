c78d869e9e738d76724fb4aa3b048dea6dc567d0
Lea Spohle <lspohle@c2s17d107.42wolfsburg.de>
2023-03-20 12:55:45 +0100

    Udpate

--- main.c
+++ main.c
@@ -6,44 +6,57 @@
 /*   By: lspohle <lspohle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/15 09:21:26 by lspohle           #+#    #+#             */
-/*   Updated: 2023/03/19 18:22:28 by lspohle          ###   ########.fr       */
+/*   Updated: 2023/03/20 12:51:07 by lspohle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-#include <sys/wait.h>
+
+int	close_files(int file_fd[2], int errnum)
+{
+	if (errnum > 0)
+		ft_printf(RED"Error: "ESC"%s\n", strerror(errno));
+	close(file_fd[0]);
+	close(file_fd[1]);
+	return(0);
+}
 
 int main(int argc, char **argv)
 {
-	int	pipefd[2];
-	char x;
+	int	pipe_fd[2];
+	int	file_fd[2];
+	int pipe_id;
 	char y;
 	
-	// if (argc == 5)
-	// 	ft_printf("1. %s\n2. %s\n3. %s\n4. %s", argv[1], argv[2], argv[3], argv[4]);
-	// ft_printf("\n");
-	if (pipe(pipefd) == -1)
+	file_fd[0] = open(argv[1], O_RDONLY);
+	file_fd[1] = open(argv[4], O_WRONLY | O_TRUNC | O_CREAT, 0644);
+	if (file_fd[0] == -1 || file_fd[1] == -1)	
+		return(close_files(file_fd, errno));
+	if (argc != 5)
+		return(0);
+	if (pipe(pipe_fd) == -1)
 		return (0);
-	int pid = fork();
-	if (pid == -1)
+	pipe_id = fork();
+	if (pipe_id == -1)
 		return(0);	
-	if (pid == 0)
+	else if (pipe_id == 0)
 	{
-		close(pipefd[0]);
-		x = argv[2][0];
-		if (write(pipefd[1], &x, sizeof(char)) == -1)
+		dup2(pipe_fd[1], 1);
+		close(pipe_fd[1]);
+		close(pipe_fd[0]);
+		if (ft_printf("%s", "HELLO WORLD") == -1)
 			return(0);
-		close(pipefd[1]);
-		ft_printf("You entered: %c\n", x);
 	}
 	else
 	{
-		close(pipefd[1]);
-		if (read(pipefd[0], &y, sizeof(char)) == -1)
-			return(0);
-		close(pipefd[0]);
-		ft_printf("Got from child process %c\n", y);
+		dup2(pipe_fd[0], 0);
+		close(pipe_fd[0]);
+		close(pipe_fd[1]);
+		ft_printf("Got from child process: ");
+		while (read(0, &y, sizeof(char)) > 0)
+			write(file_fd[1], &y, sizeof(char));
 		wait(NULL);
-	}	
-	return (0);
+	}
+	return(close_files(file_fd, 0));
+	ft_printf("%s", argv[argc]);
 }
--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: lspohle <lspohle@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/19 11:46:16 by lspohle           #+#    #+#             */
-/*   Updated: 2023/03/19 12:03:32 by lspohle          ###   ########.fr       */
+/*   Updated: 2023/03/20 12:25:21 by lspohle          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,11 @@
 # include <sys/wait.h>
 // wait
 // waitpid
+
+# define CYAN "\e[38;5;51m"
+# define GREEN "\e[38;5;47m"
+# define PURPLE "\e[38;5;219m"
+# define RED "\e[38;5;124m"
+# define ESC "\033[0m"
 
 #endif
